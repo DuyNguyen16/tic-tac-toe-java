58b8df9c8e87bfe8d474f7e52270195d374af4b8
Duy <khanhduyn161@gmail.com>
2024-05-01 22:10:08 +1000

    game rule

--- TicTacToe.java
+++ TicTacToe.java
@@ -56,25 +56,31 @@ public class TicTacToe {
           System.out.print("Player 2 enter row number:");
           row_read =  readInput.nextInt();
           
-          
+          // ask the player for the column
           System.out.print("Player 2 enter column number:");
           column_read =  readInput.nextInt();
-
+          
+          // end the loop if the inputs are valid
           if (board[row_read - 1][column_read - 1] == "_") {
             break;
           }
         }
-        
+        // add the value O to the board
         board[row_read - 1][column_read - 1] = "O";
-  
+        
+        // change the current player
         isPlayer1 = !isPlayer1;
 
       }
       
+      // clean the terminal
       System.out.print("\033[H\033[2J");
       System.out.flush();
+
+      // display the current board
       buildBoard(board);      
 
+      // check if there is a winner
       if (winner(board)) {
         if (!isPlayer1) {
           System.out.println("Player 1 win!");
@@ -89,6 +95,7 @@ public class TicTacToe {
     readInput.close();
   }
 
+  // build the board
   static void buildBoard(String[][] board) {
     for (int i = 0; i < board.length; i++) {
       for (int y = 0; y < board.length; y++) {
@@ -101,6 +108,7 @@ public class TicTacToe {
     }
   }
 
+  // game rule to find the winner
   static boolean winner(String[][] board) {
     //  check horizontally
     if ((board[0][0] == board[0][1] && board[0][1] == board[0][2]) && board[0][0] != "_") {
