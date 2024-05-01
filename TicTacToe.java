2a2e4e115c4fcd56684ce9f27efbf4d068b09630
Duy <khanhduyn161@gmail.com>
2024-05-01 21:54:37 +1000

    finish

--- TicTacToe.java
+++ TicTacToe.java
@@ -77,7 +77,35 @@ public class TicTacToe {
     }
   }
 
+  static boolean winner(String[][] board) {
+    //  check horizontally
+    if ((board[0][0] == board[0][1] && board[0][1] == board[0][2]) && board[0][0] != "_") {
+      return true;
+    } else if ((board[1][0] == board[1][1] && board[1][1] == board[1][2]) && board[1][0] != "_") {
+      return true;
+    } else if ((board[2][0] == board[2][1] && board[2][1] == board[2][2]) && board[2][0] != "_") {
+      return true;
+    }
 
+    // check vertically
+    if ((board[0][0] == board[1][0] && board[1][0] == board[2][0]) && board[0][0] != "_") {
+      return true;
+    } else if ((board[0][1] == board[1][1] && board[1][1] == board[2][1]) && board[0][1] != "_") {
+      return true;
+    } else if ((board[0][2] == board[1][2] && board[1][2] == board[2][2]) && board[0][2] != "_") {
+      return true;
+    }
+
+    // check diagonally
+
+    if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) && board[0][0] != "_") {
+      return true;
+    } else if ((board[0][2] == board[1][1] && board[1][1] == board[2][0]) && board[0][2] != "_") {
+      return true;
+    }
+
+    return false;
+  }
 }
 
 
